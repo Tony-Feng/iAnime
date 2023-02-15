3230ef647ef034169289c7de6f2c74db8baeb759
Kelly Ge <36249982+AM-Kitty@users.noreply.github.com>
2023-02-15 17:54:46 -0500

    Add Gridlayout Manager

--- app/src/main/java/com/project/ianime/screen/GalleryFragment.kt
+++ app/src/main/java/com/project/ianime/screen/GalleryFragment.kt
@@ -6,29 +6,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
 import com.project.ianime.R
+import com.project.ianime.adapter.AnimeItemAdapter
+import com.project.ianime.databinding.FragmentGalleryBinding
 
 // TODO: Consider separate filter and recyclerview layouts
 // TODO: Consider building BaseFragment
 class GalleryFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = GalleryFragment()
-    }
-
     private lateinit var viewModel: GalleryViewModel
+    private var _binding: FragmentGalleryBinding? = null
+    private val binding get()= _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_gallery, container, false)
+        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+
+        val animeCardList = binding.animeList
+        animeCardList.layoutManager = GridLayoutManager(requireContext(), 3, GridLayoutManager.VERTICAL, true)
+        val animeCardAdapter = AnimeItemAdapter {
+            // TODO: Navigate to anime detail screen
+        }
+
+        animeCardList.adapter = animeCardAdapter
+
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(GalleryViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+
+    companion object {
+        fun newInstance() = GalleryFragment()
     }
 
 }
