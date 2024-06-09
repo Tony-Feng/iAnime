9930f782de7b67c8e6776badc3b0f1691c099835
Kelly Ge <36249982+AM-Kitty@users.noreply.github.com>
2024-06-09 19:50:13 -0400

    Change to Recycler view adapter for later filter functionality

--- app/src/main/java/com/project/ianime/screens/gallery/adapter/AnimeItemAdapter.kt
+++ app/src/main/java/com/project/ianime/screens/gallery/adapter/AnimeItemAdapter.kt
@@ -1,45 +1,39 @@
 package com.project.ianime.screens.gallery.adapter
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.project.ianime.R
 import com.project.ianime.api.model.AnimeApiModel
 
 class AnimeItemAdapter(
     private val clickHandler: (String) -> Unit
-) : ListAdapter<AnimeApiModel, AnimeItemViewHolder>(DIFF_CONFIG) {
+) : RecyclerView.Adapter<AnimeItemViewHolder>() {
+
+    private var animeList = mutableListOf<AnimeApiModel>()
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun setAnimeList(animeData: List<AnimeApiModel>) {
+        this.animeList = animeData.toMutableList()
+        notifyDataSetChanged()
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AnimeItemViewHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_anime, parent, false)
-        return AnimeItemViewHolder(itemView)
+        return AnimeItemViewHolder((itemView))
+    }
+
+    override fun getItemCount(): Int {
+        return animeList.size
     }
 
     override fun onBindViewHolder(holder: AnimeItemViewHolder, position: Int) {
-        val anime = getItem(position)
-        holder.bind(anime)
+        val targetAnime = animeList[position]
+        holder.bind(targetAnime)
         // Set click item event
         holder.itemView.setOnClickListener {
-            clickHandler(anime.animeId)
-        }
-    }
-
-    companion object {
-        val DIFF_CONFIG = object : DiffUtil.ItemCallback<AnimeApiModel>() {
-            override fun areItemsTheSame(
-                oldItem: AnimeApiModel,
-                newItem: AnimeApiModel
-            ): Boolean {
-                return oldItem === newItem
-            }
-
-            override fun areContentsTheSame(
-                oldItem: AnimeApiModel,
-                newItem: AnimeApiModel
-            ): Boolean {
-                return oldItem == newItem
-            }
+            clickHandler(targetAnime.animeId)
         }
     }
 }
